10c3703bea291f34d7fc32af05be8708237a46b3
karan <karansope1234@gmail.com>
2022-03-25 21:38:24 +0530

    deploy

---
+++ server.js
@@ -0,0 +1,56 @@
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import colors from "colors";
+import morgan from "morgan";
+import connectDb from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+dotenv.config();
+const PORT = process.env.PORT || 5000;
+connectDb();
+
+const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.get("/", (req, res, next) => {
+  res.send("Api is runnning ...");
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
+app.get("/api/cofig/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
